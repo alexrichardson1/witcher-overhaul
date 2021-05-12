0390bdd984c9e9a3deaee349e97760c544cd0dc1
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-12 22:13:01 +0100

    feat(DimeritiumArmorMaterial): Add base durability

--- src/main/java/alex/witcher/overhaul/material/DimeritiumArmorMaterial.java
+++ src/main/java/alex/witcher/overhaul/material/DimeritiumArmorMaterial.java
@@ -8,6 +8,15 @@ import net.minecraft.sound.SoundEvent;
 public class DimeritiumArmorMaterial implements ArmorMaterial {
 
   public static final DimeritiumArmorMaterial INSTANCE = new DimeritiumArmorMaterial();
+  // durability
+  public static final int HELMET_DURABILITY = 13;
+  public static final int CHESTPLATE_DURABILITY = 15;
+  public static final int LEGGINGS_DURABILITY = 16;
+  public static final int BOOTS_DURABILITY = 11;
+  
+  private static final int[] BASE_DURABILITY = new int[]{HELMET_DURABILITY, CHESTPLATE_DURABILITY,
+      LEGGINGS_DURABILITY, BOOTS_DURABILITY};
+
 
   private DimeritiumArmorMaterial() {
   }
