70c65a603ff76dc4b5e17ddd3cd1d611eef7f5c2
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-14 15:46:29 +0100

    feast(DefaultBiomeFeaturesMixin): Add mixin for dimeritium_ore generation

---
+++ src/main/java/alex/witcher/overhaul/mixin/DefaultBiomeFeaturesMixin.java
@@ -0,0 +1,19 @@
+package alex.witcher.overhaul.mixin;
+
+import alex.witcher.overhaul.block.ModBlocks;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DefaultBiomeFeatures.class)
+public class DefaultBiomeFeaturesMixin {
+
+  @Inject(method = "addDefaultOres(Lnet/minecraft/world/biome/GenerationSettings$Builder;)V", at = @At("TAIL"))
+  private static void addDefaultOres(GenerationSettings.Builder builder, CallbackInfo ci) {
+    builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, ModBlocks.DIMERITIUM_ORE_OVERWORLD);
+  }
+}
