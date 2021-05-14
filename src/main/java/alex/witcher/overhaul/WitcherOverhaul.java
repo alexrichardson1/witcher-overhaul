bc12b244ac235359722be634f8877cbb22a64b49
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-14 22:06:49 +0100

    feat(WitcherOverhaul): Add ModEnchantments

--- src/main/java/alex/witcher/overhaul/WitcherOverhaul.java
+++ src/main/java/alex/witcher/overhaul/WitcherOverhaul.java
@@ -1,6 +1,7 @@
 package alex.witcher.overhaul;
 
 import alex.witcher.overhaul.block.ModBlocks;
+import alex.witcher.overhaul.enchantment.ModEnchantments;
 import alex.witcher.overhaul.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -9,6 +10,7 @@ public class WitcherOverhaul implements ModInitializer {
   @Override
   public void onInitialize() {
     ModBlocks.register();
-		ModItems.register();
+    ModEnchantments.register();
+    ModItems.register();
   }
 }
