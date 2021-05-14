d760ae7f167d2fa9015e57a19f3e808bb5ab6de1
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-14 22:04:55 +0100

    feat(ModEnchantments): Register wolven_chest enchantment

--- src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
+++ src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
@@ -1,8 +1,16 @@
 package alex.witcher.overhaul.enchantment;
 
+import alex.witcher.overhaul.utils.ModLib;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.util.registry.Registry;
+
 public class ModEnchantments {
 
+  private static final Enchantment WOLVEN_CHEST_ENCHANTMENT = WolvenChestEnchantment.INSTANCE;
+
+
   public static void register() {
-    // TODO: frost enchantment
+    Registry.register(Registry.ENCHANTMENT, ModLib.id("wolven_chest"), WOLVEN_CHEST_ENCHANTMENT);
+
   }
 }
