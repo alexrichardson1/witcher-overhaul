2bd4c863fd10a85d8a957c75aa4764d1df91be85
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-15 23:44:39 +0100

    feat(ModEnchantments): Register WOLVEN_LEGGINGS_ENCHANTMENT

--- src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
+++ src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
@@ -8,9 +8,11 @@ public class ModEnchantments {
 
   public static final Enchantment WOLVEN_CHEST_ENCHANTMENT = WolvenChestEnchantment.INSTANCE;
   public static final Enchantment WOLVEN_HELMET_ENCHANTMENT = WolvenHelmetEnchantment.INSTANCE;
+  public static final Enchantment WOLVEN_LEGGINGS_ENCHANTMENT = WolvenLeggingsEnchantment.INSTANCE;
 
   public static void register() {
     Registry.register(Registry.ENCHANTMENT, ModLib.id("wolven_armour"), WOLVEN_CHEST_ENCHANTMENT);
     Registry.register(Registry.ENCHANTMENT, ModLib.id("wolven_helmet"), WOLVEN_HELMET_ENCHANTMENT);
+    Registry.register(Registry.ENCHANTMENT, ModLib.id("wolven_leggings"), WOLVEN_LEGGINGS_ENCHANTMENT);
   }
 }
