269cc10b294da4302a632e7336a9a1554c3de81e
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-16 00:38:57 +0100

    refactor(WitcherArmourEnchantment): Extend WitcherEnchantment

--- src/main/java/alex/witcher/overhaul/enchantment/WitcherArmourEnchantment.java
+++ src/main/java/alex/witcher/overhaul/enchantment/WitcherArmourEnchantment.java
@@ -1,36 +1,17 @@
 package alex.witcher.overhaul.enchantment;
 
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 
-public abstract class WitcherArmourEnchantment extends Enchantment {
+public abstract class WitcherArmourEnchantment extends WitcherEnchantment {
 
-  protected WitcherArmourEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
-    super(weight, type, slotTypes);
-  }
-
-  @Override
-  public final int getMaxLevel() {
-    return 5;
-  }
-
-  @Override
-  public final int getMinPower(int level) {
-    return 25;
-  }
-
-  @Override
-  public final int getMaxPower(int level) {
-    return getMinPower(level) + 5;
-  }
-
-  @Override
-  protected final boolean canAccept(Enchantment other) {
-    return false;
+  protected WitcherArmourEnchantment() {
+    super(EnchantmentTarget.ARMOR,
+        new EquipmentSlot[]{EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS,
+            EquipmentSlot.FEET});
   }
 
   @Override
@@ -38,5 +19,4 @@ public abstract class WitcherArmourEnchantment extends Enchantment {
 
   @Override
   public abstract void onUserDamaged(LivingEntity user, Entity attacker, int level);
-
 }
