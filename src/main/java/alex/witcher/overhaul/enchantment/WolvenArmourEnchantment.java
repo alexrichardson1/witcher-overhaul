2c5ae98c05fe6a01f6d23639b5d749c75961346c
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-16 00:39:50 +0100

    refactor(WolvenArmourEnchantment): Remove constructor

--- src/main/java/alex/witcher/overhaul/enchantment/WolvenArmourEnchantment.java
+++ src/main/java/alex/witcher/overhaul/enchantment/WolvenArmourEnchantment.java
@@ -1,20 +1,12 @@
 package alex.witcher.overhaul.enchantment;
 
-import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 
 public class WolvenArmourEnchantment extends WitcherArmourEnchantment {
 
   public final static WolvenArmourEnchantment INSTANCE = new WolvenArmourEnchantment();
-
-  private WolvenArmourEnchantment() {
-    super(Rarity.VERY_RARE, EnchantmentTarget.ARMOR,
-        new EquipmentSlot[]{EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS,
-            EquipmentSlot.FEET});
-  }
 
   @Override
   public int getProtectionAmount(int level, DamageSource source) {
