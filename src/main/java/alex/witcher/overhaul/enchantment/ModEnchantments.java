7ae614b8828d555023addadb155c91a5ffdbf767
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-16 18:50:48 +0100

    feat(WolvenWeaponEnchantment): Register wolven_weapon

--- src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
+++ src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
@@ -6,8 +6,10 @@ import net.minecraft.util.registry.Registry;
 public class ModEnchantments {
 
   public static final WolvenArmourEnchantment WOLVEN_ARMOUR_ENCHANTMENT = WolvenArmourEnchantment.INSTANCE;
+  public static final WolvenWeaponEnchantment WOLVEN_WEAPON_ENCHANTMENT = WolvenWeaponEnchantment.INSTANCE;
 
   public static void register() {
     Registry.register(Registry.ENCHANTMENT, ModLib.id("wolven_armour"), WOLVEN_ARMOUR_ENCHANTMENT);
+    Registry.register(Registry.ENCHANTMENT, ModLib.id("wolven_weapon"), WOLVEN_WEAPON_ENCHANTMENT);
   }
 }
