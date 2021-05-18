07731fcc165cd7fe78e4eaff2338d7621d332ece
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-18 13:52:29 +0100

    refactor(DimeritiumOreBlock): Change constructor

--- src/main/java/alex/witcher/overhaul/block/DimeritiumOreBlock.java
+++ src/main/java/alex/witcher/overhaul/block/DimeritiumOreBlock.java
@@ -1,21 +1,26 @@
 package alex.witcher.overhaul.block;
 
+import alex.witcher.overhaul.utils.MiningLevel;
 import java.util.Random;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
 import net.minecraft.block.OreBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 
 public class DimeritiumOreBlock extends OreBlock {
 
   public DimeritiumOreBlock() {
-    super(FabricBlockSettings.copy(Blocks.DIAMOND_ORE));
+    super(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3, 3).sounds(
+        BlockSoundGroup.STONE)
+        .breakByTool(FabricToolTags.PICKAXES, MiningLevel.IRON.level));
   }
 
   @Override
