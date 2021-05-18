bb04d1ddceacd37f04b92cf053016b63a7e39056
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-05-18 09:25:25 +0100

    refactor(ModEnchantments): Change type of static enchantments

--- src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
+++ src/main/java/alex/witcher/overhaul/enchantment/ModEnchantments.java
@@ -1,12 +1,13 @@
 package alex.witcher.overhaul.enchantment;
 
 import alex.witcher.overhaul.utils.ModLib;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.registry.Registry;
 
 public class ModEnchantments {
 
-  public static final WolvenArmourEnchantment WOLVEN_ARMOUR_ENCHANTMENT = WolvenArmourEnchantment.INSTANCE;
-  public static final WolvenWeaponEnchantment WOLVEN_WEAPON_ENCHANTMENT = WolvenWeaponEnchantment.INSTANCE;
+  public static final Enchantment WOLVEN_ARMOUR_ENCHANTMENT = WolvenArmourEnchantment.INSTANCE;
+  public static final Enchantment WOLVEN_WEAPON_ENCHANTMENT = WolvenWeaponEnchantment.INSTANCE;
 
   public static void register() {
     Registry.register(Registry.ENCHANTMENT, ModLib.id("wolven_armour"), WOLVEN_ARMOUR_ENCHANTMENT);
