b07e107acd5f3e74ac3fa6e7a2c8a814756b4b16
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-07-29 15:00:09 +0100

    refactor(WolvenWeaponEnchantment): Remove onTargetDamaged

--- src/main/java/alex/witcher/overhaul/enchantment/WolvenWeaponEnchantment.java
+++ src/main/java/alex/witcher/overhaul/enchantment/WolvenWeaponEnchantment.java
@@ -1,25 +1,8 @@
 package alex.witcher.overhaul.enchantment;
 
-import alex.witcher.overhaul.statuseffect.ModStatusEffects;
-import alex.witcher.overhaul.utils.ModLib;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityGroup;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
 
 public class WolvenWeaponEnchantment extends WitcherWeaponEnchantment {
-
-  @Override
-  public void onTargetDamaged(LivingEntity user, Entity target, int level) {
-    if (target instanceof LivingEntity) {
-      ((LivingEntity) target).addStatusEffect(
-          new StatusEffectInstance(ModStatusEffects.BLEED, ModLib.TICKS_PER_SECOND * 2 * level,
-              level - 1));
-    }
-  }
-
-  // +11% Critical Hit Chance
-  // +20% Exp from Monsters
 
   @Override
   public float getAttackDamage(int level, EntityGroup group) {
