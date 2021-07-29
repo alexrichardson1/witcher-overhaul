d839beec9f84722e5663277276b520599a027e1e
Alex Richardson <alex.richardson19@imperial.ac.uk>
2021-07-29 14:56:58 +0100

    refactor(WitcherWeaponEnchantment): Remove onTargetDamaged

--- src/main/java/alex/witcher/overhaul/enchantment/WitcherWeaponEnchantment.java
+++ src/main/java/alex/witcher/overhaul/enchantment/WitcherWeaponEnchantment.java
@@ -1,19 +1,14 @@
 package alex.witcher.overhaul.enchantment;
 
 import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
 
 public abstract class WitcherWeaponEnchantment extends WitcherEnchantment {
 
   protected WitcherWeaponEnchantment() {
     super(EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
   }
-
-  @Override
-  public abstract void onTargetDamaged(LivingEntity user, Entity target, int level);
 
   @Override
   public abstract float getAttackDamage(int level, EntityGroup group);
